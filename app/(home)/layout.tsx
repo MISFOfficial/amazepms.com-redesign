dd232508e1977dc46ad109992d5fc7b174773381
MISFOfficial <muksitul44@gmail.com>
2026-07-30 00:55:56 +0600

    feat: implement and integrate responsive Footer component into base layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "../Global/Navigation/Navigation";
+import Footer from "../Global/Footer/Footer";
 
 function BaseLayout({ children }: { children: React.ReactNode }) {
   return (
     <main>
       <Navigation />
       {children}
+      <Footer />
     </main>
   );
 }
